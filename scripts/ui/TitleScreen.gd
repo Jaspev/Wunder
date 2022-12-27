b5b700ea287fe35c195dd908dd7310828032805d
Jaspev <jacksonpeck12@gmail.com>
2022-12-27 14:14:01 -0700

    fontagain

--- scripts/ui/TitleScreen.gd
+++ scripts/ui/TitleScreen.gd
@@ -1,4 +1,14 @@
 extends Control
 
 func _ready():
-	$CenterContainer/VBoxContainer/Label/ButtonStart.grab_focus()
+	$VBoxContainer/LabelStart/ButtonStart.grab_focus()
+
+func _process(delta):
+	if $VBoxContainer/LabelStart/ButtonStart.has_focus():
+		$Selector.position = Vector2(600, 94)
+	
+	if $VBoxContainer/LabelOptions/ButtonOptions.has_focus():
+		$Selector.position = Vector2(819, 186)
+	
+	if $VBoxContainer/LabelQuit/ButtonQuit.has_focus():
+		$Selector.position = Vector2(484, 286)

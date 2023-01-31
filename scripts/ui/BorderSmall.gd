6fc75d42ddab085755f7f574a7df3b5c77d1acea
Jaspev <jacksonpeck12@gmail.com>
2023-01-30 22:18:50 -0700

    cover thing for magic man

--- scripts/ui/BorderSmall.gd
+++ scripts/ui/BorderSmall.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
+var cover = Sprite.new()
+var texwhite = preload("res://textures/ui/whitepixel.png")
+
 func _ready():
-	var cover = MeshInstance2D.new()
-	cover.set_mesh(QuadMesh)
-	cover.scale.x = 7
-	cover.scale.y = 7
+	cover.set_texture(texwhite)
+	cover.scale.x = 208
+	cover.scale.y = 208
 	yield(get_tree().create_timer(2.5),"timeout")
 	add_child(cover)
